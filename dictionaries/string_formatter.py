8a9fe19339089a05a8554e6578c111d7ec8bd484
dark-hermes <hermawansent@gmail.com>
2021-03-15 14:36:57 +0700

    add some comments in english.py and string_formatter.py

--- dictionaries/string_formatter.py
+++ dictionaries/string_formatter.py
@@ -1,16 +1,20 @@
 import re
 
+# define a function to select just word with alphabet character
 def no_strip(word):
     if re.search("^[a-zA-Z].*[a-zA-Z]$", word):
         return True
     else:
         return False
 
+# if letter length <= 5 
 def level_1(word):
     return len(word) <= 5
 
+# if letter length > 5 and not contain any whitespaces
 def level_2(word):
     return len(word) > 5 and not ' ' in word
 
+# if the word contain any whitespaces
 def level_3(word):
     return ' ' in word
